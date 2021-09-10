b14e441d690653dd5b50b03b8d63adb8a9d1984c
Robert J Kirk <rk@robertjkirk.com>
2021-09-09 23:14:50 -0400

    added header values for telemetry csv

--- TVC_Software_3.x/src/setup/csv_headers.hpp
+++ TVC_Software_3.x/src/setup/csv_headers.hpp
@@ -3,7 +3,7 @@
 
 
 void csvHeaderPrint() {
-    Serial.print(F("Milliseconds since power on"));
+    Serial.print(F("Millis"));
     Serial.print(F(","));
     Serial.print(F("Date/Time"));
     Serial.print(F(","));
@@ -13,7 +13,73 @@ void csvHeaderPrint() {
     Serial.print(F(","));
     Serial.print(F("Corrected Euler Y"));
     Serial.print(F(","));
-    Serial.print(F("Corrected Euler Z"));
+    Serial.print(F("Filtered Altitude"));
+    Serial.print(F(","));
+    Serial.print(F("Temperature"));
+    Serial.print(F(","));
+    Serial.print(F("Lattitude"));
+    Serial.print(F(","));
+    Serial.print(F("Longitude"));
+    Serial.print(F(","));
+    Serial.print(F("GPS Altitude"));
+    Serial.print(F(","));
+    Serial.print(F("Sat Fix"));
+    Serial.print(F(","));
+    Serial.print(F("Pyro 1 Status")); 
+    Serial.print(F(","));
+    Serial.print(F("Pyro 2 Status"));
+    Serial.print(F(","));
+    Serial.print(F("Raw Euler X"));
+    Serial.print(F(","));
+    Serial.print(F("Raw Euler Y")); 
+    Serial.print(F(","));
+    Serial.print(F("Raw Euler Z"));
+    Serial.print(F(","));
+    Serial.print(F("Quaternion X"));
+    Serial.print(F(","));
+    Serial.print(F("Quaternion Y"));
+    Serial.print(F(","));
+    Serial.print(F("Quaternion Z")); 
+    Serial.print(F(","));
+    Serial.print(F("Quaternion W"));
+    Serial.print(F(","));
+    Serial.print(F("Gimbal Euler X"));
+    Serial.print(F(","));
+    Serial.print(F("Gimbal Euler Y"));
+    Serial.print(F(","));
+    Serial.print(F("Gimbal Euler Z"));
+    Serial.print(F(","));
+    Serial.print(F("Gimbal Quaternion X"));
+    Serial.print(F(","));
+    Serial.print(F("Gimbal Quaternion Y"));
+    Serial.print(F(","));
+    Serial.print(F("Gimbal Quaternion Z"));
+    Serial.print(F(","));
+    Serial.print(F("Gimbal Quaternion W"));
+    Serial.print(F(","));
+    Serial.print(F("Unfiltered Altitude"));
+    Serial.print(F(","));
+    Serial.print(F("Pressure"));
+    Serial.print(F(","));
+    Serial.print(F("PID Setpoint Y"));
+    Serial.print(F(","));
+    Serial.print(F("PID Setpoint Z"));
+    Serial.print(F(","));
+    Serial.print(F("Gimbal Home Y"));
+    Serial.print(F(","));
+    Serial.print(F("Gimbal Home Z"));
+    Serial.print(F(","));
+    Serial.print(F("PID Proportional"));
+    Serial.print(F(","));
+    Serial.print(F("PID Integral"));
+    Serial.print(F(","));
+    Serial.print(F("PID Derivative"));
+    Serial.print(F(","));
+    Serial.print(F("Kalman Meas. Uncertainty"));
+    Serial.print(F(","));
+    Serial.print(F("Kalman Est. Uncertainty"));
+    Serial.print(F(","));
+    Serial.print(F("Kalman Process Variance"));
 
 
 }
